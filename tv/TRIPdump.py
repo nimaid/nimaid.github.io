10b7c10f9f0a959dc879b33b6dda022344db7ef9
Ella Jameson <ellagjameson@gmail.com>
2019-11-12 14:20:17 -0700

    Removed redundant file imports, started work on trip db updater helper script

--- tv/TRIPdump.py
+++ tv/TRIPdump.py
@@ -8,15 +8,15 @@ import reddit_videos
 
 subreddit = 'trippyvideos'
 
-folder_dir = os.path.dirname(os.path.realpath(__file__))
-
-dump_name = folder_dir + '/trip_database_dump.csv'
-database_name = folder_dir + '/trip_database.json'
-
 print("r/TrippyVideos Dumper by nimaid (made for trip.nimaid.com)")
 
-import TRIPdbclean # Prune the main DB
+import TRIPdbclean  # Prune the main DB
 print("")
+
+folder_dir = TRIPdbclean.folder_dir
+database_name = folder_dir + TRIPdbclean.database_name
+
+dump_name = folder_dir + '/trip_database_dump.csv'
 
 all_vids = []
 print("Reading existing dump...")
@@ -24,14 +24,14 @@ with open(dump_name, 'r') as f:
     reader = csv.reader(f)
     all_vids = [i[0] for i in list(reader)]
 exist_size = len(all_vids)
-print("Read " + str(exist_size) + " OLD video IDs!\n")
+print("Read " + str(exist_size) + " valid OLD video IDs!\n")
 
 print("Getting new videos...")
 for x in reddit_videos.get_max_all(subreddit, prune=False):
     all_vids.append(x)
 all_vids = list(set(all_vids))
 new_size = len(all_vids)
-print("Got " + str(new_size - exist_size) + " new ID's! Now there are " + str(new_size) + " IDs.\n")
+print("Got " + str(new_size - exist_size) + " new ID's!\n")
 
 print("Pruning bad video ID's...")
 valid_vids = yt.remove_bad_ids(all_vids)
@@ -39,11 +39,9 @@ prune_size = len(valid_vids)
 print("Removed " + str(new_size - prune_size) + " bad IDs, only " + str(prune_size) + " remaining.\n")
 
 print("Removing ID's in main database from dump...")
-json_file = dict()
-with open(database_name, 'r') as f:
-    json_file = json.load(f)
-valid_vids = [x for x in valid_vids if x not in json_file["videos"]]
-valid_vids = [x for x in valid_vids if x not in json_file["bad_videos"]]
+clean_json = TRIPdbclean.json_file
+valid_vids = [x for x in valid_vids if x not in clean_json["videos"]]
+valid_vids = [x for x in valid_vids if x not in clean_json["bad_videos"]]
 final_size = len(valid_vids)
 print("Removed " + str(prune_size - final_size) + " existing IDs, only " + str(final_size) + " remaining.\n")
 
---
+++ tv/TRIPupdate.py
@@ -0,0 +1,7 @@
+import csv
+import json
+import os
+
+import TRIPdump  # Clean main db and update CSV
+
+#TODO: Prompt if you want to start auditing videos
