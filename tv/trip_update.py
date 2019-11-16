26b0b87dd9d48e8de462816dcc6d5de255e14d8e
Ella Jameson <ellagjameson@gmail.com>
2019-11-15 20:24:03 -0700

    Added progress saving to trip updater

--- tv/trip_update.py
+++ tv/trip_update.py
@@ -33,50 +33,54 @@ def ask_question(question="Y/N", options=["Y", "N"], default=None):
             else:
                 print(error_string, options)
 
+def save_progress(json_database, csv_dump):
+    json_database["updated"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    with open(database_name, 'w') as f:
+        json.dump(json_database, f)
+    
+    with open(dump_name, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows([[i] for i in csv_dump])
+
+
 print("")
 vet_videos = ask_question("Would you like to vet dumped videos now? Y/N")
 unvetted_vids = []
-videos_left_to_vet = valid_vids_size
 if(vet_videos == "Y"):
     print("Vetting videos now! Enter 'Q' to exit...\n")
     for i, vid in enumerate(valid_vids):
-        print(videos_left_to_vet, "videos left! Current ID:", vid)
+        print(valid_vids_size - i, "videos left! Current ID:", vid)
         vid_verdict = ask_question("Is this (G)ood, (B)ad, or should I (S)kip it? G/B/[S]/Q",
                                    options=["G", "B", "S", "Q"],
                                    default="S")
         if vid_verdict == "G":
             print("Adding to good videos..")
             clean_json["videos"].append(vid)
-            print("Added", vid, "to good videos!\n")
+            print("Added", vid, "to good videos!")
         elif vid_verdict == "B":
             print("Adding to bad videos..")
             clean_json["bad_videos"].append(vid)
-            print("Added", vid, "to bad videos!\n")
+            print("Added", vid, "to bad videos!")
         elif vid_verdict == "Q":
             print("Stopping vetting process...\n")
-            for v in valid_vids[i:]:
-                unvetted_vids.append(v)
+            unvetted_vids += valid_vids[i:]
             break;
         else:
-            print("Skipping ", vid, "...\n")
-        videos_left_to_vet -= 1
+            print("Skipping", vid, "...")
+            unvetted_vids.append(vid)
+        
+        save_progress(clean_json, unvetted_vids + valid_vids[i+1:])
+        print("Saved progress.\n")
     
     vetted_g_vids_size = len(clean_json["videos"])
     vetted_b_vids_size = len(clean_json["bad_videos"])
-
-    print("Vetting done. Saving database to file...")
-    clean_json["updated"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    with open(database_name, 'w') as f:
-        json.dump(clean_json, f)
-    print("Saved", vetted_g_vids_size, "videos to the database!\n")
-
-    print("Saving unvetted videos...")
-    with open(dump_name, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows([[i] for i in unvetted_vids])
     unvetted_size = len(unvetted_vids)
-    print("Saved", unvetted_size, "unvetted videos!\n")
+
+    print("Vetting done. Saving files...")
+    save_progress(clean_json, unvetted_vids)
+    print("Saved", vetted_g_vids_size, "videos to the JSON database!")
+    print("Saved", unvetted_size, "unvetted videos to the CSV dump!\n")
     
-    print("All done!", vetted_g_vids_size - old_json_size, "new videos added to database!", vetted_g_vids_size, "total,", unvetted_size, "unvetted.")  
+    print("All done!", vetted_g_vids_size - old_json_size, "new good videos added to database!", vetted_g_vids_size, "total,", unvetted_size, "unvetted.")  
 else:
     print("Not vetting videos!", final_json_size, "videos in database,", valid_vids_size, "unvetted.")
